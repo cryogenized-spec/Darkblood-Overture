aedd499c04002385cbf78c51ad5b554052f21033
cryogenized-spec <cryogenized@gmail.com>
2026-08-29 18:15:01 +0200

    chore: add Queen content placeholder

---
+++ src/data/queen.js
@@ -0,0 +1,18 @@
+export const QUEEN = {
+  id: 'queen',
+  displayName: 'THE QUEEN',
+  archetype: 'Necromancer',
+  visual: {
+    ageRead: 'late-20s',
+    skin: 'tanned Mediterranean complexion',
+    hair: 'long black hair',
+    eyes: 'azure / gold',
+    lips: 'black',
+    ears: 'demonic pointed ears',
+    teeth: 'subtle canine tips',
+  },
+  equipment: {
+    primary: 'ancient staff',
+  },
+  animationSockets: ['idle', 'walk', 'cast', 'hurt', 'death'],
+};
