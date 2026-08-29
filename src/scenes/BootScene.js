21cc5697489f3e2024717c8980a26430cbf9fdad
cryogenized-spec <cryogenized@gmail.com>
2026-08-29 20:23:34 +0200

    fix: preload presentation artwork in boot scene

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,8 +1,16 @@
 import Phaser from 'phaser';
 
+const DEV_SPLASH_KEY = 'dev-splash-art';
+const TITLE_ART_KEY = 'title-screen-art';
+
 export class BootScene extends Phaser.Scene {
   constructor() {
     super('BootScene');
+  }
+
+  preload() {
+    this.load.image(DEV_SPLASH_KEY, 'assets/ui/dev-splash/obsidian-moon-studio-splash.png');
+    this.load.image(TITLE_ART_KEY, 'assets/ui/title-screen/darkblood-overture-title.png');
   }
 
   create() {
