714f107d8621cabf625f66d194024bf1b3d83d45
cryogenized-spec <cryogenized@gmail.com>
2026-08-29 19:14:36 +0200

    feat: preload Arabella awakening sprite sheet

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -5,6 +5,14 @@ export class BootScene extends Phaser.Scene {
     super('BootScene');
   }
 
+  preload() {
+    this.load.spritesheet('arabella-preact1', '/assets/characters/arabella/PREACT1_QUEEN.png', {
+      frameWidth: 96,
+      frameHeight: 112,
+      endFrame: 12,
+    });
+  }
+
   create() {
     this.scene.start('DevSplashScene');
   }
