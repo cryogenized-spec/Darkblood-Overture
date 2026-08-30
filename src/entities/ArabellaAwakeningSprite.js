d5c43306b0f14879350c2fcda42d2cc4ac2aa34a
cryogenized-spec <cryogenized@gmail.com>
2026-08-30 05:02:29 +0200

    fix: bind Arabella sprite to prepared texture instances

--- src/entities/ArabellaAwakeningSprite.js
+++ src/entities/ArabellaAwakeningSprite.js
@@ -7,17 +7,30 @@ import {
 
 export class ArabellaAwakeningSprite extends Phaser.GameObjects.Sprite {
   constructor(scene, x, y) {
-    super(scene, x, y, ARABELLA_TEXTURE_KEYS.dormant);
+    const textureKey = ARABELLA_TEXTURE_KEYS.dormant;
+    const texture = scene.textures.get(textureKey);
+    if (!texture) {
+      throw new Error(`Arabella texture '${textureKey}' was not prepared before AwakeningScene.`);
+    }
+
+    super(scene, x, y, texture);
     scene.add.existing(this);
     this.setOrigin(0.5, 1);
     this.setDepth(20);
     this.setScale(1);
+    this.setVisible(true);
+    this.setAlpha(1);
     this.baseHeight = ARABELLA_PIXEL.height;
   }
 
   frame(name) {
     const textureKey = ARABELLA_TEXTURE_KEYS[name];
-    if (textureKey) this.setTexture(textureKey);
+    if (textureKey) {
+      const texture = this.scene.textures.get(textureKey);
+      if (!texture) throw new Error(`Arabella texture '${textureKey}' is missing.`);
+      this.setTexture(texture);
+      this.setVisible(true);
+    }
   }
 
   setLunarCharge(active) {
