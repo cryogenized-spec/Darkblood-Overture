d963413c0dabca4e677f806063a583b4b6a3863c
cryogenized-spec <cryogenized@gmail.com>
2026-08-30 17:23:36 +0200

    tune: reduce awakening sprite scale

--- src/data/arabellaAwakeningFrames.js
+++ src/data/arabellaAwakeningFrames.js
@@ -1,4 +1,4 @@
-export const ARABELLA_SPRITE_DISPLAY_HEIGHT = 112;
+export const ARABELLA_SPRITE_DISPLAY_HEIGHT = 80;
 
 export const ARABELLA_AWAKENING_FRAMES = Object.freeze([
   { name: 'dormant', file: '01-dormant.png' },
