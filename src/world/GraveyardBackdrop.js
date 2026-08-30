79e1d3a57bfe6fbd8bde679c72b2e5f7726db927
cryogenized-spec <cryogenized@gmail.com>
2026-08-30 18:58:29 +0200

    fix: let camera drive graveyard parallax

--- src/world/GraveyardBackdrop.js
+++ src/world/GraveyardBackdrop.js
@@ -1,20 +1,21 @@
-import { GAME_WIDTH, GAME_HEIGHT } from '../config/gameConfig.js';
+import { GAME_HEIGHT, GAME_WIDTH } from '../config/gameConfig.js';
 
 const LAYER_SPEEDS = Object.freeze({
   far: 0.12,
   mid: 0.36,
   near: 0.72,
 });
+const BACKDROP_WIDTH = 2240;
 
 function drawRepeated(layer, drawMotif) {
-  for (let offset = 0; offset < GAME_WIDTH * 3; offset += GAME_WIDTH) {
+  for (let offset = 0; offset < BACKDROP_WIDTH; offset += GAME_WIDTH) {
     drawMotif(layer, offset);
   }
 }
 
 function drawFar(layer) {
   layer.fillStyle(0x0b0911, 1);
-  layer.fillRect(0, 0, GAME_WIDTH * 3, GAME_HEIGHT);
+  layer.fillRect(0, 0, BACKDROP_WIDTH, GAME_HEIGHT);
   layer.fillStyle(0x171320, 1);
   drawRepeated(layer, (g, x) => {
     g.fillTriangle(x + 8, 72, x + 48, 35, x + 92, 72);
@@ -24,14 +25,14 @@ function drawFar(layer) {
     g.fillRect(x + 148, 42, 4, 30);
   });
   layer.fillStyle(0x241d2d, 1);
-  layer.fillRect(0, 72, GAME_WIDTH * 3, 28);
+  layer.fillRect(0, 72, BACKDROP_WIDTH, 28);
   layer.fillStyle(0x3a2d43, 0.35);
   layer.fillCircle(255, 34, 20);
 }
 
 function drawMid(layer) {
   layer.fillStyle(0x09080d, 1);
-  layer.fillRect(0, 92, GAME_WIDTH * 3, 88);
+  layer.fillRect(0, 92, BACKDROP_WIDTH, 88);
   layer.fillStyle(0x211827, 1);
   drawRepeated(layer, (g, x) => {
     g.fillRect(x + 18, 84, 8, 40);
@@ -43,12 +44,12 @@ function drawMid(layer) {
     g.fillTriangle(x + 148, 78, x + 155, 66, x + 162, 78);
   });
   layer.fillStyle(0x302338, 1);
-  layer.fillRect(0, 124, GAME_WIDTH * 3, 6);
+  layer.fillRect(0, 124, BACKDROP_WIDTH, 6);
 }
 
 function drawNear(layer) {
   layer.fillStyle(0x0c0910, 1);
-  layer.fillRect(0, 128, GAME_WIDTH * 3, 52);
+  layer.fillRect(0, 128, BACKDROP_WIDTH, 52);
   layer.fillStyle(0x18131d, 1);
   drawRepeated(layer, (g, x) => {
     g.fillRect(x + 10, 118, 4, 22);
@@ -61,31 +62,22 @@ function drawNear(layer) {
     g.fillRect(x + 172, 120, 12, 3);
   });
   layer.fillStyle(0x28202d, 1);
-  layer.fillRect(0, 142, GAME_WIDTH * 3, 4);
+  layer.fillRect(0, 142, BACKDROP_WIDTH, 4);
 }
 
 export function createGraveyardBackdrop(scene) {
   const layers = {
-    far: scene.add.graphics().setDepth(0),
-    mid: scene.add.graphics().setDepth(1),
-    near: scene.add.graphics().setDepth(2),
+    far: scene.add.graphics().setDepth(0).setScrollFactor(LAYER_SPEEDS.far, 1),
+    mid: scene.add.graphics().setDepth(1).setScrollFactor(LAYER_SPEEDS.mid, 1),
+    near: scene.add.graphics().setDepth(2).setScrollFactor(LAYER_SPEEDS.near, 1),
   };
 
   drawFar(layers.far);
   drawMid(layers.mid);
   drawNear(layers.near);
 
-  const originX = scene.player?.x ?? GAME_WIDTH / 2;
-
   return {
     layers,
-    originX,
-    update(playerX) {
-      const displacement = playerX - originX;
-      layers.far.x = -displacement * LAYER_SPEEDS.far;
-      layers.mid.x = -displacement * LAYER_SPEEDS.mid;
-      layers.near.x = -displacement * LAYER_SPEEDS.near;
-    },
     destroy() {
       Object.values(layers).forEach((layer) => layer.destroy());
     },
